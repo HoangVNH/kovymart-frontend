366af67b358d7e48c97a777bb2596e06f3f030db
hoangnguyen <nmhoang.28@gmail.com>
2021-09-19 19:04:35 -0700

    Fix empty default address

--- src/features/address/addressSlice.js
+++ src/features/address/addressSlice.js
@@ -75,7 +75,7 @@ const addressSlice = createSlice({
       .addCase(getAddressList.fulfilled, (state, action) => {
         state.requesting = false
         state.list_address = action.payload.data.data
-        state.default_address = action.payload.data.data[0]
+        state.default_address = action.payload.data.data[0] ? action.payload.data.data[0] : {}
       })
       .addCase(updateAddress.fulfilled, (state) => {
         state.requesting = false
